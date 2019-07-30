c940a549988689917b0a01e0647159b138a10ad4
wenqihuang <walker.wong@outlook.com>
2019-07-30 10:28:48 +0800

    rewrite main

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import numpy as np
 from CSReconstruct import cs_reconstruct
 
 if __name__ == "__main__":
+
+    '''
+    ### simulation
     img = cv2.imread("img/sample_1024pixel.bmp",0)
     img = cv2.imread("img/test.jpeg",0)
     img = cv2.imread("img/timg2.jpeg",0)
@@ -18,10 +21,10 @@ if __name__ == "__main__":
 
     # prepare roi mask
     roi_mask = np.zeros((height_high,width_high))
-    roi_l = int(width_high/3.0)
-    roi_r = int(2.0*width_high/3.0)
-    roi_u = int(height_high/3.0)
-    roi_d = int(2.0*height_high/3.0)
+    roi_l = int(width_high * 0.33)
+    roi_r = int(2.0*width_high * 0.33)
+    roi_u = int(height_high * 0.33)
+    roi_d = int(2.0*height_high * 0.33)
     roi_mask[roi_u:roi_d,roi_l:roi_r] = 1
 
     height_roi = roi_d - roi_u
@@ -32,10 +35,14 @@ if __name__ == "__main__":
 
     
 
-    cs_rate = 0.6
-    n_high = height_high*width_high
+    cs_rate = 1
+    n_high = height_high * width_high
     n_roi = height_roi * width_roi
     m_high = int(cs_rate * n_roi)
+
+    print(height_roi,width_roi)
+    
+
 
     n_low = height_low * width_low
     m_low = int(cs_rate * n_low)
@@ -81,7 +88,90 @@ if __name__ == "__main__":
     #print(np.max(phi_roi))
     #cv2.imwrite("phi_roi.jpg", phi_roi*255)
     #cv2.waitKey(0)
+    '''
+    ##################
 
+    
+    img = cv2.imread("img/sample_1024pixel.bmp",0)
+    img = cv2.imread("img/test.jpeg",0)
+    img = cv2.imread("img/timg2.jpeg",0)
+    #img = cv2.imread("/Users/huangwenqi/Pictures/IMGP0726-1.jpg",0)
+
+    cs_rate = 1
+    (height_coarse, width_coarse) = (60, 80)
+    (height_fine, width_fine) = (180, 240)
+    img_hi = cv2.resize(img, (width_fine, height_fine))
+    img_lo = cv2.resize(img, (width_coarse, height_coarse))
+    #print(img_hi.shape)
+
+    #(x,y,x,y)
+    roi_bbox = np.array((int(0.33*height_fine), int(0.33*width_fine),\
+        int(0.66*height_fine), int(0.66*width_fine)))
+
+    height_roi = roi_bbox[2] - roi_bbox[0]
+    width_roi = roi_bbox[3] - roi_bbox[1]
+
+    
+
+    n_coarse_full = width_coarse * height_coarse
+    n_fine_full = width_fine * height_fine
+    n_fine_roi = width_roi * height_roi
+
+    m_coarse = int(n_coarse_full * cs_rate)
+    m_fine = int(n_fine_roi * cs_rate)
+
+    phi_coarse = (np.random.rand(m_coarse, n_coarse_full) > 0.5).astype(int)
+    phi_fine = (np.random.rand(m_fine, n_fine_roi) > 0.5).astype(int)
+
+    masks_coarse = np.zeros((height_coarse,width_coarse,m_coarse))
+    masks_fine = np.zeros((height_fine, width_fine,m_fine))
+
+    #print(phi_coarse)
+    print(phi_coarse[0,:])
+    tmp = np.zeros((height_coarse,width_coarse))
+    for i in range(m_coarse):
+        tmp = phi_coarse[i,:].reshape((height_coarse,width_coarse))
+        masks_coarse[:,:,i] = tmp
+        
+
+    tmp = np.zeros((height_roi,width_roi))
+    for i in range(m_fine):
+        tmp = phi_fine[i,:].reshape((height_roi,width_roi))
+        masks_fine[roi_bbox[0]:roi_bbox[2],roi_bbox[1]:roi_bbox[3],i] = tmp
+        
+    
+    #sample simulation
+    y_coarse = np.zeros((m_coarse,1))
+    y_fine = np.zeros((m_fine,1))
+    for i in range(m_coarse):
+        y_coarse[i,0] = (masks_coarse[:,:,i] * img_lo).sum()
+    
+    for i in range(m_fine):
+        y_fine[i,0] = (masks_fine[:,:,i] * img_hi).sum()
+
+    '''
+    print(m_high,n_high)
+    print(m_fine,n_fine_full)
+    print(y_fine.shape,phi_fine.shape,height_roi,width_roi)
+    print(y_high.shape,phi_roi.shape,height_roi,width_roi)
+    print(np.max(y_fine),np.max(y_coarse))
+    print(np.max(y_high),np.max(y_low))
+    print(np.max(masks_coarse), np.max(masks_fine))
+    print(height_roi,width_roi)
+    
+    '''
+    print("Begin reconstruct ROI with high resolution.")
+    reimg_roi = cs_reconstruct(y_fine,phi_fine,height_roi,width_roi,cs_rate)
+    print("ROI reconstruction finished.")
+    print("Begin reconstruct low-resolution image.")
+    reimg_low = cs_reconstruct(y_coarse,phi_coarse,height_coarse,width_coarse,cs_rate)
+    print("low-resolution reconstruction finished.")
+    reimg = cv2.resize(reimg_low, (height_fine,width_fine), interpolation=cv2.INTER_NEAREST)
+    reimg[roi_bbox[0]:roi_bbox[2],roi_bbox[1]:roi_bbox[3]] = reimg_roi
+    
+    
+    
+    '''
     print("Begin reconstruct ROI with high resolution.")
     reimg_roi = cs_reconstruct(y_high,phi_roi,height_roi,width_roi,cs_rate)
     print("ROI reconstruction finished.")
@@ -90,6 +180,7 @@ if __name__ == "__main__":
     print("low-resolution reconstruction finished.")
     reimg = cv2.resize(reimg_low, (height_high,width_high), interpolation=cv2.INTER_NEAREST)
     reimg[roi_u:roi_d,roi_l:roi_r] = reimg_roi
+    '''
 
     
     cv2.imshow("origin image",img)
